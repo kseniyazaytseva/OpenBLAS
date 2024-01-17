7071f01f3633a2bfd07e5dc92bc91d80d4d55779
Andrey Sokolov <andrey.sokolov@syntacore.com>
2024-01-17 19:26:20 +0300

    Fix BLAS and LAPACK tests for C910V and RISCV64_ZVL256B targets
    * Fixed bugs in _vector files for C910V and RISCV64_ZVL256B targets
    * Added additional definitions for RISCV64_ZVL256B target

--- kernel/riscv64/dsdot_vector.c
+++ kernel/riscv64/dsdot_vector.c
@@ -1,17 +1,28 @@
 /***************************************************************************
-Copyright (C), 2023, KNS Group LLC (YADRO).
-
-All Rights Reserved.
-
-
-This software contains the intellectual property of YADRO or is licensed to YADRO
-
-from third parties. Use of this software and the intellectual property contained
-
-therein is expressly limited to the terms and conditions of the License Agreement
-
-under which it is provided by YADRO.
-
+Copyright (c) 2023, The OpenBLAS Project
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
+the documentation and/or other materials provided with the
+distribution.
+3. Neither the name of the OpenBLAS project nor the names of
+its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE OPENBLAS PROJECT OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************/
 
 #include "common.h"
@@ -34,8 +45,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 gvl = vsetvl_e64m4(n);
                 vr = vfmv_v_f_f64m4(0, gvl);
                 for(i=0,j=0; i<n/gvl; i++){
-                        vx = vle_v_f32m2(&x[j], gvl);
-                        vy = vle_v_f32m2(&y[j], gvl);
+                        vx = vle32_v_f32m2(&x[j], gvl);
+                        vy = vle32_v_f32m2(&y[j], gvl);
                         vr = vfwmacc_vv_f64m4(vr, vx, vy, gvl);
                         j += gvl;
                 }
@@ -46,8 +57,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 //tail
                 if(j < n){
                         gvl = vsetvl_e64m4(n-j);
-                        vx = vle_v_f32m2(&x[j], gvl);
-                        vy = vle_v_f32m2(&y[j], gvl);
+                        vx = vle32_v_f32m2(&x[j], gvl);
+                        vy = vle32_v_f32m2(&y[j], gvl);
                         vfloat64m4_t vz = vfmv_v_f_f64m4(0, gvl);
                         //vr = vfdot_vv_f32m2(vx, vy, gvl);
                         vr = vfwmacc_vv_f64m4(vz, vx, vy, gvl);
@@ -59,8 +70,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 vr = vfmv_v_f_f64m4(0, gvl);
                  int stride_x = inc_x * sizeof(FLOAT);
                 for(i=0,j=0; i<n/gvl; i++){
-                        vx = vlse_v_f32m2(&x[j*inc_x], stride_x, gvl);
-                        vy = vle_v_f32m2(&y[j], gvl);
+                        vx = vlse32_v_f32m2(&x[j*inc_x], stride_x, gvl);
+                        vy = vle32_v_f32m2(&y[j], gvl);
                         vr = vfwmacc_vv_f64m4(vr, vx, vy, gvl);
                         j += gvl;
                 }
@@ -72,8 +83,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 //tail
                 if(j < n){
                         gvl = vsetvl_e64m4(n-j);
-                        vx = vlse_v_f32m2(&x[j*inc_x], stride_x, gvl);
-                        vy = vle_v_f32m2(&y[j], gvl);
+                        vx = vlse32_v_f32m2(&x[j*inc_x], stride_x, gvl);
+                        vy = vle32_v_f32m2(&y[j], gvl);
                         vfloat64m4_t vz = vfmv_v_f_f64m4(0, gvl);
                         //vr = vfdot_vv_f32m2(vx, vy, gvl);
                         vr = vfwmacc_vv_f64m4(vz, vx, vy, gvl);
@@ -86,8 +97,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 vr = vfmv_v_f_f64m4(0, gvl);
                  int stride_y = inc_y * sizeof(FLOAT);
                 for(i=0,j=0; i<n/gvl; i++){
-                        vx = vle_v_f32m2(&x[j], gvl);
-                        vy = vlse_v_f32m2(&y[j*inc_y], stride_y, gvl);
+                        vx = vle32_v_f32m2(&x[j], gvl);
+                        vy = vlse32_v_f32m2(&y[j*inc_y], stride_y, gvl);
                         vr = vfwmacc_vv_f64m4(vr, vx, vy, gvl);
                         j += gvl;
                 }
@@ -99,8 +110,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 //tail
                 if(j < n){
                         gvl = vsetvl_e64m4(n-j);
-                        vx = vle_v_f32m2(&x[j], gvl);
-                        vy = vlse_v_f32m2(&y[j*inc_y], stride_y, gvl);
+                        vx = vle32_v_f32m2(&x[j], gvl);
+                        vy = vlse32_v_f32m2(&y[j*inc_y], stride_y, gvl);
                         vfloat64m4_t vz = vfmv_v_f_f64m4(0, gvl);
                         //vr = vfdot_vv_f32m2(vx, vy, gvl);
                         vr = vfwmacc_vv_f64m4(vz, vx, vy, gvl);
@@ -114,8 +125,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                  int stride_x = inc_x * sizeof(FLOAT);
                  int stride_y = inc_y * sizeof(FLOAT);
                 for(i=0,j=0; i<n/gvl; i++){
-                        vx = vlse_v_f32m2(&x[j*inc_x], stride_x, gvl);
-                        vy = vlse_v_f32m2(&y[j*inc_y], stride_y, gvl);
+                        vx = vlse32_v_f32m2(&x[j*inc_x], stride_x, gvl);
+                        vy = vlse32_v_f32m2(&y[j*inc_y], stride_y, gvl);
                         vr = vfwmacc_vv_f64m4(vr, vx, vy, gvl);
                         j += gvl;
                 }
@@ -127,8 +138,8 @@ double CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x, FLOAT *y, BLASLONG inc_y)
                 //tail
                 if(j < n){
                         gvl = vsetvl_e64m4(n-j);
-                        vx = vlse_v_f32m2(&x[j*inc_x], stride_x, gvl);
-                        vy = vlse_v_f32m2(&y[j*inc_y], stride_y, gvl);
+                        vx = vlse32_v_f32m2(&x[j*inc_x], stride_x, gvl);
+                        vy = vlse32_v_f32m2(&y[j*inc_y], stride_y, gvl);
                         vfloat64m4_t vz = vfmv_v_f_f64m4(0, gvl);
                         //vr = vfdot_vv_f32m2(vx, vy, gvl);
                         vr = vfwmacc_vv_f64m4(vz, vx, vy, gvl);
